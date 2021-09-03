626e29a68c41d1c3c02a083339c79df3a4a3182e
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2021-09-02 23:40:17 -0700

    fixed test

--- tests/test_queryparser.py
+++ tests/test_queryparser.py
@@ -1,4 +1,4 @@
-from odinson.ruleutils import QueryParser
+from odinson.ruleutils import parse_odinson_query
 from odinson.ruleutils.config import HOLE_GLYPH
 from odinson.ruleutils.queryast import HoleSurface
 import pyparsing
@@ -7,9 +7,8 @@ import unittest
 
 class TestQueryParser(unittest.TestCase):
     def test_parse(self):
-        qp = QueryParser()
-        res = qp.parse(HOLE_GLYPH)
+        res = parse_odinson_query(HOLE_GLYPH)
         self.assertTrue(isinstance(res, HoleSurface))
         # this should fail
         with self.assertRaises(pyparsing.ParseException):
-            qp.parse("¯\(°_o)/¯")
+            parse_odinson_query("¯\(°_o)/¯")
